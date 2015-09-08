faf194f9a3688c9d2649a4b34ff58ea6057e410f
rho <ryan@theoleschool.com>
2015-09-08 12:09:40 -0500

    initial commit of angular codebase

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ichabodngApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ichabodngApp
+ */
+angular.module('ichabodngApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ichabodngApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ichabodngApp
+ */
+angular.module('ichabodngApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
